18d794283a0ebd33238dabcac9f7561d38427338
Joice Gomes <joice.gomes@sap.com>
2019-09-17 19:48:45 -0300

    Creating login screen

--- src/ui/screens/login/login.screen.tsx
+++ src/ui/screens/login/login.screen.tsx
@@ -1,16 +1,105 @@
 import React, { PureComponent } from 'react'
-import { Link } from 'react-router-dom'
-import { Screens } from 'constants/index'
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Screens } from 'constants/index';
+
+import './login.css';
 
 interface LoginProps { }
+
+function handleClick() {
+  <Link href={Screens.HOME}></Link>
+};
 
 export class LoginScreen extends PureComponent<LoginProps, {}> {
   render(): JSX.Element {
     return (
-      <div>
-        <Link to={Screens.HOME}>to home</Link>
-      </div>
-    )
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div>          
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button onClick={handleClick}
+              type="submit"
+              fullWidth
+              variant="contained"
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    );
   }
 }
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    color: '#3F72333',
+  },
+}));
 
