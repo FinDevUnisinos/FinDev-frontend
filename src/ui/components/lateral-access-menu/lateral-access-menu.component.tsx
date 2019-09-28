cb4f6255496ec3e6502e4e81604b1f8b74d2ce80
Pedro Bohlmann Cascaes Silva <pedro.b.c.silva@gmail.com>
2019-09-27 21:16:28 -0300

    refact(LateralAccessMenuComponent): code review

--- src/ui/components/lateral-access-menu/lateral-access-menu.component.tsx
+++ src/ui/components/lateral-access-menu/lateral-access-menu.component.tsx
@@ -46,8 +46,6 @@ export class LateralAccessMenuComponent extends PureComponent<LateralAccessMenuP
     })
   }
 
-
-
   renderContent(): JSX.Element {
     return !this.props.isRetracted ? (
       <div className="lateral-access-menu-content">
