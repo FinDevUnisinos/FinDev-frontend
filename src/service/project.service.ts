ac1470a0d01f5828d70fdf1b8dbceff935b72d57
augustoaccorsi <augusto.acorsi@gmail.com>
2019-12-09 17:50:09 -0300

    remove getProjectsByOwner

--- src/service/project.service.ts
+++ src/service/project.service.ts
@@ -119,18 +119,4 @@ export default class ProjectService {
     })
   }
 
-  static getProjectsByOwner(userId: number){
-    return axios({
-      method: 'POST',
-      url: `${CONFIG.API_URL}/project/all`,
-      headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': localStorage.token
-      },
-      data: {
-        id: userId
-      },
-    })
-  }
-
   }
